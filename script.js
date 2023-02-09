2c1b97a19c243d7eb1de085cf50b474fbbc0b7a9
ÂIsel Castro <iselcq@gmail.com>
2023-02-09 14:01:09 -0600

    feat: crear 6 tarjetas iniciales con JS

--- script.js
+++ script.js
@@ -13,14 +13,18 @@ const btnNewCard = document.querySelector("#add-button");
 const newCardClose = document.querySelector(".new-place__close");
 const newCardSubmit = document.querySelector(".new-place__submit");
 const newCard = document.querySelector(".new-place");
+const newCardTitle = document.querySelector(".cards__location");
+const newCardImage = document.querySelector(".cards__image");
+const inputTitle = document.querySelector("#new-title");
+const inputImage = document.querySelector("#new-url");
 
 //funciones para editar perfil
 function openPopUp() {
+  formElement.classList.add("pop-up__opened");
   const name = profileName.textContent;
   inputName.value = name;
   const profession = profileJob.textContent;
   inputJob.value = profession;
-  formElement.classList.add("pop-up__opened");
 }
 
 function closePopUp() {
@@ -55,3 +59,62 @@ function closeCard() {
 
 newCardClose.addEventListener("click", closeCard);
 btnNewCard.addEventListener("click", newCardOpen);
+
+function handleNewCardSubmitForm(evt) {
+  evt.preventDefault();
+
+  const titleValue = inputTitle.value;
+  const imageValue = inputImage.value;
+
+  newCardTitle.textContent = titleValue;
+  newCardImage.textContent = imageValue;
+  closeCard();
+}
+
+const cardData = [
+  {
+    src: "images/cannonBeach.png",
+    alt: "Imagen de Cannon Beach con una gran roca al fondo",
+    title: "Cannon Beach",
+  },
+  {
+    src: "images/whiteSands.png",
+    alt: "Imagen de un campo de dunas blancas en Nuevo Mexico",
+    title: "White Sands",
+  },
+  {
+    src: "images/kilauea.jpg",
+    alt: "Imagen de la erupción en el volcán Kilauea",
+    title: "Volcán Kilauea",
+  },
+  {
+    src: "images/bodieIsland.jpg",
+    alt: "Imagen del Faro de Bodie Island encendido",
+    title: "Bodie Island Lighthouse",
+  },
+  {
+    src: "images/antelopeCanyon.png",
+    alt: "Imagen dentro de los cañones de ranura en Arizona",
+    title: "Cañón del Antílope",
+  },
+  {
+    src: "images/haleakala.png",
+    alt: "Imagen de la cima del volcán Haleakala con niebla",
+    title: "Haleakalā",
+  },
+];
+
+cardData.forEach((element) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardTemplateClone = cardTemplate
+    .querySelector(".cards__card")
+    .cloneNode(true);
+  const cardContainer = document.querySelector(".cards");
+
+  cardTemplateClone.querySelector(".cards__image").src = element.src;
+  cardTemplateClone.querySelector(".cards__image").alt = element.alt;
+  cardTemplateClone.querySelector(".cards__location").textContent =
+    element.title;
+
+  cardContainer.append(cardTemplateClone);
+});
