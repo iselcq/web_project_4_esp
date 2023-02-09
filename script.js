8145174c47bb0da15796c251b85d925aff2c748c
ÂIsel Castro <iselcq@gmail.com>
2023-02-09 13:19:07 -0600

    feat: crear modal para agregar nueva tarjeta

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+//variables para formulario editar profile
 const formElement = document.querySelector(".pop-up");
-const newCardElement = document.querySelector(".new-place");
 const buttonOpen = document.querySelector(".profile__edit-button");
 const buttonAdd = document.querySelector(".profile__add-button");
 const buttonClose = document.querySelector(".pop-up__close");
 const buttonSubmit = document.querySelector(".pop-up__submit");
-const newCardClose = document.querySelector(".new-place__close");
-const newCardSubmit = document.querySelector(".new-place__submit");
-
 const inputName = document.querySelector("#user-name");
 const inputJob = document.querySelector("#user-profession");
-
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__profession");
+//variables para formulario de nueva tarjeta
+const btnNewCard = document.querySelector("#add-button");
+const newCardClose = document.querySelector(".new-place__close");
+const newCardSubmit = document.querySelector(".new-place__submit");
+const newCard = document.querySelector(".new-place");
 
+//funciones para editar perfil
 function openPopUp() {
   const name = profileName.textContent;
   inputName.value = name;
@@ -41,3 +43,15 @@ function handleProfileSubmitForm(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileSubmitForm); //Es lo mismo
+
+//funciones para agregar nueva tarjeta
+function newCardOpen() {
+  newCard.classList.add("new-place__opened");
+}
+
+function closeCard() {
+  newCard.classList.remove("new-place__opened");
+}
+
+newCardClose.addEventListener("click", closeCard);
+btnNewCard.addEventListener("click", newCardOpen);
