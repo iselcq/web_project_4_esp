41376fbb50251cfa546215b86c4db6e0064a926d
ÂIsel Castro <iselcq@gmail.com>
2023-03-14 11:26:57 -0600

    hotfix: agregar funcion de cerrar imagen con click y tecla esc

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,12 +4,14 @@ const btnNewCard = document.querySelector("#add-button");
 const newCardClose = document.querySelector(".new-place__close");
 const newCard = document.querySelector(".new-place");
 const formElement = document.querySelector(".pop-up");
+const imagePopup = document.querySelector(".image-popup");
 const profileName = document.querySelector(".profile__name");
 const inputName = document.querySelector("#user-name");
 const profileJob = document.querySelector(".profile__profession");
 const inputJob = document.querySelector("#user-profession");
 const popupElement = document.querySelector(".pop-up__card");
 const cardElement = document.querySelector(".new-place__card");
+const imageElement = document.querySelector(".image-popup__section");
 //funciones para abrir y cerrar modal para nueva tarjeta
 function newCardOpen() {
   newCard.classList.add("new-place__opened");
@@ -38,13 +40,22 @@ function closePopUp() {
 buttonOpen.addEventListener("click", openPopUp);
 buttonClose.addEventListener("click", closePopUp);
 
+//funcion para cerrar imagen
+function closeImage() {
+  imagePopup.classList.remove("image-popup__opened");
+}
+
 //cerrar modales con tecla "esc"
-document.addEventListener("keydown", function (evt, formElement, newCard) {
-  if (evt.key === "Escape") {
-    closePopUp(formElement);
-    closeCard(newCard);
+document.addEventListener(
+  "keydown",
+  function (evt, formElement, newCard, imagePopup) {
+    if (evt.key === "Escape") {
+      closePopUp(formElement);
+      closeCard(newCard);
+      closeImage(imagePopup);
+    }
   }
-});
+);
 
 // //cerrar modales con un click fuera del area
 formElement.addEventListener("click", function (popupElement) {
@@ -56,11 +67,18 @@ popupElement.addEventListener("click", (evt) => {
 });
 
 //cerrar modal con un click fuera del area
-
 newCard.addEventListener("click", function (newCard) {
   closeCard(newCard);
 });
 
 cardElement.addEventListener("click", (evt) => {
+  evt.stopPropagation();
+});
+
+//cerrar modal imagen con un click fuera del area
+imagePopup.addEventListener("click", function (imagePopup) {
+  closeImage(imagePopup);
+});
+imageElement.addEventListener("click", (evt) => {
   evt.stopPropagation();
 });
